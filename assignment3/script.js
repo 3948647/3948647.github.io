e32d6a7dd44a9c19ecdf9e11f67ac5ffb4c76ede
mialayne <130018260+3948647@users.noreply.github.com>
2024-06-06 22:56:37 +1000

    hu

--- assignment3/script.js
+++ assignment3/script.js
@@ -28,3 +28,67 @@ driverdetails.addEventListener("click", function() {
   var hiddenText = document.getElementById("hiddenText"); 
   hiddenText.style.display = "block"; 
 }); 
+
+const charlesleclerc = document.querySelector("#charlesleclerc");
+console.log(charlesleclerc);
+
+const moreInfo = document.querySelector("#more-info");
+console.log(moreInfo);
+
+charlesleclerc.addEventListener("mouseover", showMoreInfo);
+charlesleclerc.addEventListener("mouseout", hideMoreInfo);
+
+function showMoreInfo() {
+  moreInfo.classList.add("show");
+}
+
+function hideMoreInfo() {
+  moreInfo.classList.remove("show");
+}
+// const hoverButton = document.querySelector("#hover-button");
+// console.log(hoverButton);
+
+// const moreInfo = document.querySelector("#more-info");
+// console.log(moreInfo);
+
+// hoverButton.addEventListener("mouseover", showMoreInfo);
+// hoverButton.addEventListener("mouseout", hideMoreInfo);
+
+// function showMoreInfo() {
+//   moreInfo.classList.add("show");
+// }
+
+// function hideMoreInfo() {
+//   moreInfo.classList.remove("show");
+// }
+
+let draggedElement = null;
+
+const colorBox = document.querySelector(".colorbox");
+const dropBox = document.querySelector(".dropbox");
+
+colorBox.addEventListener("dragstart", startDrag);
+
+function startDrag() {
+  draggedElement = colorBox;
+}
+
+dropBox.addEventListener("dragover", endDrag);
+
+function endDrag(event) {
+  event.preventDefault();
+}
+
+dropBox.addEventListener("drop", handleDrop);
+
+function handleDrop() {
+  if (draggedElement) {
+    const color = window
+      .getComputedStyle(draggedElement)
+      .getPropertyValue("background-color");
+    // const color = draggedElement.style.backgroundColor;
+    dropBox.style.backgroundColor = color;
+    dropBox.textContent = "Dropped!";
+    draggedElement = null;
+  }
+}
--- week10/hover/script.js
+++ week10/hover/script.js
@@ -1,23 +1,16 @@
-const catImg = document.querySelector("cat-img");
-console.log(catImg);
+const hoverButton = document.querySelector("#hover-button");
+console.log(hoverButton);
 
-cosnt more-Info = document.querySelector("more-info");
+const moreInfo = document.querySelector("#more-info");
 console.log(moreInfo);
 
-catImg.addEventListener("mouseover", showInfo);
-catImg.addEventListener("mouseout", hideInfo);
+hoverButton.addEventListener("mouseover", showMoreInfo);
+hoverButton.addEventListener("mouseout", hideMoreInfo);
 
-moreInfo.innerHTML ="<p>Add to cart </p>";
-
-function showInfo()
-{ 
-    moreInfo.classList.add("show");
-    moreInfo.style.backgroundColor = "red";
-
+function showMoreInfo() {
+  moreInfo.classList.add("show");
 }
 
-function hideInfo()
-{ 
-    moreInfo.classList.remove("show");
-    
+function hideMoreInfo() {
+  moreInfo.classList.remove("show");
 }
