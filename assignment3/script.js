164041626208ffd07ae4e0de11e59ee291498309
mialayne <130018260+3948647@users.noreply.github.com>
2024-06-09 22:58:56 +1000

    today

--- assignment3/script.js
+++ assignment3/script.js
@@ -96,17 +96,6 @@ function handleDrop() {
 // scroll 
 const scrollContent = document.querySelector("#scroll-content");
 
-const hscrollButton = document.querySelector("#hscroll-button");
-console.log(hscrollButton);
-
-hscrollButton.addEventListener("click", getHScroll);
-
-const hscroll = document.querySelector("#hscroll");
-
-function getHScroll() {
-  hscroll.textContent = scrollContent.scrollLeft;
-}
-
 const vscrollButton = document.querySelector("#vscroll-button");
 console.log(vscrollButton);
 
@@ -118,15 +107,6 @@ function getVScroll() {
   vscroll.textContent = scrollContent.scrollTop;
 }
 
-const hscroll150Button = document.querySelector("#hscroll150-button");
-console.log(hscroll150Button);
-
-hscroll150Button.addEventListener("click", gotoHScroll150);
-
-function gotoHScroll150() {
-  scrollContent.scrollTo({ left: 150, behavior: "smooth" });
-}
-
 const vscroll300Button = document.querySelector("#vscroll300-button");
 console.log(vscroll300Button);
 
@@ -134,15 +114,4 @@ vscroll300Button.addEventListener("click", gotoVScroll300);
 
 function gotoVScroll300() {
   scrollContent.scrollTo({ top: 300, behavior: "smooth" });
-}
-
-const wvscrollButton = document.querySelector("#wvscroll-button");
-console.log(wvscrollButton);
-
-wvscrollButton.addEventListener("click", getWVScroll);
-
-const wvscroll = document.querySelector("#wvscroll");
-
-function getWVScroll() {
-  wvscroll.textContent = window.scrollY;
 }
